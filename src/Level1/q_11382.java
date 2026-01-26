b4dc9d3ec840af41df5fca44dbeef1e6145a579c
hyewon-S <shw12421@hanmail.net>
2026-01-27 02:07:34 +0900

    1/27 commit

--- src/Level1/q_11382.java
+++ src/Level1/q_11382.java
@@ -1,7 +1,20 @@
 package Level1;
+import java.util.Scanner;
 
 public class q_11382 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+//        int num1 = sc.nextInt();
+//        int num2 = sc.nextInt();
+//        int num3 = sc.nextInt();
+        //long 타입으로 담아야 함
+
+        long num1 = sc.nextLong();
+        long num2 = sc.nextLong();
+        long num3 = sc.nextLong();
+
+        System.out.println(num1 + num2 + num3);
+        sc.close();
     }
 }
