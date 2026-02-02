df058dabfca097994a00411ec26bc5b653a21d00
hyewon-S <shw12421@hanmail.net>
2026-02-02 16:43:44 +0900

    2/2 commit

---
+++ src/Level3/q_10950.java
@@ -0,0 +1,21 @@
+package Level3;
+
+/*
+아직 제출 전
+ */
+import java.util.Scanner;
+
+public class q_10950 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a + b);
+        }
+        sc.close();
+    }
+}
