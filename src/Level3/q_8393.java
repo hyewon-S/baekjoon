17afe89935221a0cc490d51981faa9ae375ddc4c
hyewon-S <shw12421@hanmail.net>
2026-03-12 15:36:45 +0900

    3/12 commit

--- src/Level3/q_8393.java
+++ src/Level3/q_8393.java
@@ -1,18 +1,18 @@
 package Level3;
-
 import java.util.Scanner;
 
-public class q_10950 {
+public class q_8393 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
+        int sum = 0;
 
-        for (int i = 0; i < n; i++) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            System.out.println(a + b);
+        for(int i = 1; i <= n; i++) {
+            sum += i;
         }
+
+        System.out.println(sum);
         sc.close();
     }
 }
