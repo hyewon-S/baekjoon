59c533aeb8a458e018342f45a5b57e4c9a11a87e
hyewon-S <shw12421@hanmail.net>
2026-03-18 23:59:33 +0900

    3/18 commit

---
+++ src/Level10/q_15894.java
@@ -0,0 +1,14 @@
+package Level10;
+
+import java.util.Scanner;
+
+public class q_15894 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+
+        System.out.println(n + 3 * n);
+        sc.close();
+    }
+}
